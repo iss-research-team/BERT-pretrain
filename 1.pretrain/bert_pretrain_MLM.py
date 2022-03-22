22a8abd497b239f3e25594585798f1dbbc64d578
logan0116 <1809361150@qq.com>
2022-03-22 17:42:23 +0800

    logan-0322-update

--- 1.pretrain/bert_pretrain_MLM.py
+++ 1.pretrain/bert_pretrain_MLM.py
@@ -7,26 +7,27 @@
 
 
 import torch
-from transformers import AlbertConfig, AlbertForMaskedLM, AlbertTokenizer
+from transformers import AutoModelForMaskedLM, AutoTokenizer
 from transformers import DataCollatorForLanguageModeling, LineByLineTextDataset, TrainingArguments, Trainer
 
-bert_file = "albert-base"
+bert_file = "roberta-base"
+label = 'patent'
 
-config = AlbertConfig.from_pretrained(bert_file)
-model = AlbertForMaskedLM.from_pretrained(bert_file)
-tokenizer = AlbertTokenizer.from_pretrained(bert_file)
+model = AutoModelForMaskedLM.from_pretrained(bert_file)
+tokenizer = AutoTokenizer.from_pretrained(bert_file)
 print('No of parameters: ', model.num_parameters())
 
-dataset = LineByLineTextDataset(tokenizer=tokenizer, file_path="../data/input/train.txt", block_size=512)
+dataset = LineByLineTextDataset(tokenizer=tokenizer, file_path="../data/input/cnc_doc_" + label + ".txt",
+                                block_size=512)
 data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=True, mlm_probability=0.15)
 print('No. of lines: ', len(dataset))
 
 training_args = TrainingArguments(
-    output_dir='../data/outputs/',
+    output_dir='../data/output_' + label,
     overwrite_output_dir=True,
     num_train_epochs=50,
     per_device_train_batch_size=4,
-    save_steps=100000,
+    save_steps=10000,
 )
 trainer = Trainer(
     model=model,
@@ -36,4 +37,4 @@ trainer = Trainer(
 )
 
 trainer.train()
-trainer.save_model('./outputs/')
+trainer.save_model('../data/output_' + label)
